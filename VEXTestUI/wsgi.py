c74aac4fc8f10dfe498cb776679c74d2d15d2ed7
Yanyang <yanyang.xie@thistech.com>
2016-08-08 12:14:53 +0800

    add config

--- VEXTestUI/wsgi.py
+++ VEXTestUI/wsgi.py
@@ -1,3 +1,5 @@
+# -*- coding=utf-8 -*-
+
 """
 WSGI config for VEXTestUI project.
 
@@ -8,9 +10,12 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
+# 将系统的编码设置为UTF8
+reload(sys)
+sys.setdefaultencoding('utf8')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "VEXTestUI.settings")
 
 application = get_wsgi_application()
